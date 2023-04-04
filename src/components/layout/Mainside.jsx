a62f2e02d64ea0db77480d0a6e2031bf6d0967a9
Miquéias Castro Soares <107892315+miqueiassoares@users.noreply.github.com>
2023-04-04 19:05:22 -0300

    finalizando o rightside

--- src/components/layout/Mainside.jsx
+++ src/components/layout/Mainside.jsx
@@ -1,9 +1,6 @@
-import asdf from "../../assets/img/rightside/filmes/supernatural.png";
-
 const Mainside = () => {
   return(
     <div className="mainside">
-      <img src={asdf} alt="" />
     </div>
   )
 }
