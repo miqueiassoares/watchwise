37b2f71208ae9a34e6d8af5b08eefd8ae3cec89e
Miquéias Castro Soares <107892315+miqueiassoares@users.noreply.github.com>
2023-04-06 20:43:03 -0300

    finalizando o recuo dos lados

--- src/App.jsx
+++ src/App.jsx
@@ -18,29 +18,56 @@ const useClosedOpen = () => {
 }
 
 
-
 const App = () => {
   const [rightSide, setRightSide] = useClosedOpen();
   const [leftSide, setLeftSide] = useClosedOpen();
   const [width, setWidth] = useState(window.innerWidth);
+  const [classe, setClasse] = useState("");
+
+  
   useEffect(() => {
-    if(width <= 800) {
-      setLeftSide(false);
+    if (!leftSide && !leftSide) {
+      setClasse("twoSides");
+    }  else if (leftSide) {
+      setClasse("leftAberto");
       setRightSide(false);
+    } else {
+      setClasse("");
     }
-  }, []);
+  }, [leftSide]);
+  
+  useEffect(() => {
+    if (width > 800 && !rightSide) {
+      setClasse("rightrecolhidoFull");
+    } else if (!leftSide && !rightSide) {
+      setClasse("twoSides");
+    } else if (width <= 800 && rightSide) {
+      setClasse("rightAberto");
+      setLeftSide(false);
+    } else {
+      setClasse("");
+    }
+  }, [rightSide]);
+  
   window.addEventListener("resize", () => {
     setWidth(window.innerWidth);
     if (width <= 800) {
-      setLeftSide(false);
-      setRightSide(false);
+      setClasse("twoSides");
     } else {
-      setLeftSide(true);
-      setRightSide(true);
+      setClasse("");
     }
   });
+
+  useEffect(()=> {
+    if (width <= 800) {
+      setLeftSide(false);
+      setRightSide(false);
+      setClasse("twoSides");
+    }
+  }, []);
+
   return (
-    <div className={`App ${!leftSide ? "leftrecolhido" : ""}  ${!rightSide ? "rightrecolhido" : ""}`}>
+    <div className={`App  ${classe}`}>
       <Leftside  handleClosed={setLeftSide} />
       <Mainside/>
       <Rightside handleClosed= {setRightSide} />
