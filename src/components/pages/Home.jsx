9971f0d5b4abba70d9ec6a477916b98a489cb682
Miquéias Castro Soares <107892315+miqueiassoares@users.noreply.github.com>
2023-04-08 23:55:42 -0300

    search

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,7 +1,35 @@
+import { Link } from "react-router-dom";
+
+import search from "../../assets/img/mainside/home/Search.svg";
+import filter from "../../assets/img/mainside/home/Filter.svg";
+
 const Home = () => {
   return (
     <div className="home">
-      home
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link>Movies</Link>
+            </li>
+            <li>
+              <Link className="active">TV Shows</Link>
+            </li>
+            <li>
+              <Link>Anime</Link>
+            </li>
+          </ul>
+        </nav>
+        <form>
+          <label htmlFor="Search">
+            <img src={search} alt="Search" />
+          </label>
+          <input type="text" name="Search" id="Search" placeholder="Search" />
+          <button>
+            <img src={filter} alt="Filter" />
+          </button>
+        </form>
+      </header>
     </div>
   );
 }
