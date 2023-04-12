e03ceba4d388416d3e876e3c32ae2ee775fcb319
Miquéias Castro Soares <107892315+miqueiassoares@users.noreply.github.com>
2023-04-12 19:24:47 -0300

    exportando header

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,36 +1,10 @@
-import { Link } from "react-router-dom";
-
-import search from "../../assets/img/mainside/home/Search.svg";
-import filter from "../../assets/img/mainside/home/Filter.svg";
 import TvShows from "./home/TvShows";
+import Header from "./home/Header";
 
 const Home = () => {
   return (
     <div className="home">
-      <header>
-        <nav>
-          <ul>
-            <li>
-              <Link>Movies</Link>
-            </li>
-            <li>
-              <Link className="active">TV Shows</Link>
-            </li>
-            <li>
-              <Link>Anime</Link>
-            </li>
-          </ul>
-        </nav>
-        <form autoComplete="off">
-          <label htmlFor="Search">
-            <img src={search} alt="Search" />
-          </label>
-          <input type="text" name="Search" id="Search" placeholder="Search" />
-          <button>
-            <img src={filter} alt="Filter" />
-          </button>
-        </form>
-      </header>
+      <Header/>
       <TvShows />
     </div>
   );
